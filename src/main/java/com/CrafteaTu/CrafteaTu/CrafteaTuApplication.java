a1a73ef0901194e47f75f9dd7cd72118498b2444
Luisotb <jluisoterobarreiro@gmail.com>
2021-05-23 18:08:50 +0200

    Holuwu

---
+++ src/main/java/com/CrafteaTu/CrafteaTu/CrafteaTuApplication.java
@@ -0,0 +1,13 @@
+package com.CrafteaTu.CrafteaTu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrafteaTuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrafteaTuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CrafteaTu/CrafteaTu/CrafteaTuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CrafteaTu.CrafteaTu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrafteaTuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
